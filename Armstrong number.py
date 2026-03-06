e265fb9c09ff2f774b0ff9fac11f3507be6d4b07
SIVARANJANI.S <sivaranjani1072006@gmail.com>
2026-03-06 14:04:07 +0530

    Add files via upload

---
+++ Armstrong number.py
@@ -0,0 +1,11 @@
+n=int(input())
+temp=n
+sum=0
+while temp>0:
+    digit = temp%10
+    sum+=digit**3
+    temp//=10
+if sum==n:
+    print("True")
+else:
+    print("False")
---
+++ Square root and Floor value.py
@@ -0,0 +1,7 @@
+n=int(input())
+i=1
+print(n**0.5)
+while i*i <n:
+    i+=1
+    print(i-1)
+    
